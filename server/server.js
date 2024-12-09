759ee4be2a30434947488c1bed0f2112b7dcf02c
Amauri <hamauri@aol.com>
2024-12-09 12:48:45 -0800

    updated server to dist folder

--- server/server.js
+++ server/server.js
@@ -17,13 +17,13 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+// if we're in production, serve client/dist as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
 }
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 // Create a new instance of an Apollo server with the GraphQL schema
