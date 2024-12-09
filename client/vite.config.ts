247c4b5a850c3099ca57a19f11e5040945cf0310
Amauri <hamauri@aol.com>
2024-12-09 11:46:33 -0800

    setup vite proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,5 +6,25 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    proxy: {
+      '/graphql': {
+        target: 'http://localhost:3001',
+        configure: (proxy) => {
+          proxy.on('error', (err) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (_, req) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req) => {
+            console.log(
+              'Received Response from the Target:',
+              proxyRes.statusCode,
+              req.url
+            );
+          });
+        },
+      },
+    },
   },
 });
