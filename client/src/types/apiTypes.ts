ea588a9baf0983ecb95a7536c38aa14c95b8797a
Amauri <hamauri@aol.com>
2024-12-09 16:56:00 -0800

    updating typing files

--- client/src/types/apiTypes.ts
+++ client/src/types/apiTypes.ts
@@ -1,24 +1,35 @@
-export type UserType = {
-  _id: string;
-  username: string;
-};
-export type MedicineType = {
-  _id: string;
-  name: string;
-  dosage: number;
-  amount: number;
-  interval: string;
-  subInterval: string;
-  times: [string];
-  queue: [QueueType];
-  isActive: boolean;
-  userId: string;
-};
-
-export type QueueType = {
-  _id: string;
+export type IdType = string;
+export type UsernameType = string;
+export type NameType = string;
+export type DosageType = number;
+export type AmountType = number;
+export type IntervalType = string;
+export type SubIntervalType = string;
+export type TimesType = string[];
+export type QueueItemType = {
+  _id: IdType;
   time: string;
   checked?: boolean;
+};
+export type QueueType = QueueItemType[];
+export type IsActiveType = boolean;
+export type UserIdType = string;
+
+export type UserType = {
+  _id: IdType;
+  username: UsernameType;
+};
+export type MedicineType = {
+  _id: IdType;
+  name: NameType;
+  dosage: DosageType;
+  amount: AmountType;
+  interval: IntervalType;
+  subInterval: SubIntervalType;
+  times: TimesType;
+  queue: QueueType;
+  isActive: IsActiveType;
+  userId: UserIdType;
 };
 
 export type AuthType = {
@@ -32,21 +43,24 @@ export type MedicineInputType = {
   amount?: number;
   interval?: string;
   subInterval?: string;
-  times?: [string];
+  times?: string[];
 };
 
 export type QueueInputType = {
   time: string;
 };
 
-export type QueryType = {
-  medicine(medicineId: string): MedicineType;
-  medicines?: [MedicineType];
+export type MedicineQueryType = {
+  medicine: MedicineType;
+};
+
+export type MedicinesQueryType = {
+  medicines: MedicineType[];
 };
 
 export type MutationType = {
-  addUser(username: string, password: string): AuthType;
-  login(username: string, password: string): AuthType;
+  addUser(username: UsernameType, password: string): AuthType;
+  login(username: UsernameType, password: string): AuthType;
   addMedicine(medicine: MedicineInputType): MedicineType;
   updateMedicine(medicineId: string, medicine: MedicineInputType): MedicineType;
   toggleIsActive(medicineId: string): MedicineType;
