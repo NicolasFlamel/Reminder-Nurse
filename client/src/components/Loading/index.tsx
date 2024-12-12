83e2c90521337d8fb12b23b691102add05dddbca
Amauri <hamauri@aol.com>
2024-12-12 09:35:06 -0800

    created LoadingMedicationList component

--- client/src/components/Loading/index.tsx
+++ client/src/components/Loading/index.tsx
@@ -1,4 +1,5 @@
-import { CSSProperties } from 'react';
+import './styles.css';
+import { CSSProperties, ReactNode } from 'react';
 
 export const Loading = () => {
   const styles: CSSProperties = { display: 'flex', justifyContent: 'center' };
@@ -7,5 +8,21 @@ export const Loading = () => {
     <section style={styles}>
       <h1>Loading</h1>
     </section>
+  );
+};
+
+interface LoadingMedicationListProps {
+  children?: ReactNode;
+}
+
+export const LoadingMedicationList = ({
+  children,
+}: LoadingMedicationListProps) => {
+  return (
+    <div className="loading-medication-list">
+      <div className="loading-spinner">
+        <p className="spinner-text">{children ? children : 'Loading...'}</p>
+      </div>
+    </div>
   );
 };
